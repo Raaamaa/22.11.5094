3fa6a5874abc97138fe48e99227207d00e82e213
Ramadhani <128121383+Raaamaa@users.noreply.github.com>
2023-04-02 16:25:02 +0700

    Add files via upload
    Nama : Akhmad Rizki Ramadhani 
    Nim : 22.11.5094

---
+++ Taxi.cs
@@ -0,0 +1,36 @@
+﻿using Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiApp;
+
+class Taxi
+{
+
+    //properties
+    public string DriverName { get; set; }
+    public bool OnDuty { get; set; }
+    public int NumPass { get; set; }
+
+    //method
+    public void taxiInfo()
+    {
+        Console.WriteLine("Driver Name: {0}", DriverName);
+        Console.WriteLine("On Duty: {0}", OnDuty);
+        Console.WriteLine("Number of Passenger: {0}", NumPass);
+    }
+  
+    public void pickUpPassenger()
+    {
+        Console.WriteLine("{0} sedang menjemput penumpang\n", DriverName);
+    }
+
+    public void dropOffPassenger()
+    {
+        Console.WriteLine("{0} selesai mengantar penumpang\n", DriverName);
+    }
+    
+}
---
+++ main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiApp;
+
+namespace Main
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            //set nilai properties
+            taxi.DriverName = "Juan";
+            taxi.OnDuty = true;
+            taxi.NumPass = 10;
+
+            //pemanggilan method
+            taxi.taxiInfo();
+
+            if (taxi.OnDuty == true)
+            {
+                taxi.pickUpPassenger();
+            }
+            else if (taxi.OnDuty == false)
+            {
+                taxi.dropOffPassenger();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
